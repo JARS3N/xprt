af7cb741a3eb32bd842ad7ab26aa29cc014966e5
JARS3N <JARS3N@users.noreply.github.com>
2017-10-20 07:51:08 -0400

    paste0 switched for paste intended

--- R/open_save_xl_wb.R
+++ R/open_save_xl_wb.R
@@ -1,7 +1,7 @@
-open_save_xl_wb<-function(u=getwd()){
+open_save_xl_wb2<-function(u=getwd()){
   system(
-      paste0(
-      "Cscript ",
+    paste(
+      "Cscript",
       shQuote(normalizePath(system.file("/vbs/open_and_save_xl_workbooks.vbs", package="xprt"))),
       shQuote(normalizePath(u)),
       sep=" "
