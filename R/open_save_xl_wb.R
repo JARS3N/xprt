60f351f2e294692df12b49feb4acd4e26a4f3501
JARS3N <JARS3N@users.noreply.github.com>
2017-10-20 07:48:27 -0400

    Update open_save_xl_wb.R

--- R/open_save_xl_wb.R
+++ R/open_save_xl_wb.R
@@ -1,7 +1,7 @@
 open_save_xl_wb<-function(u=getwd()){
   system(
       paste0(
-      "Cscript",
+      "Cscript ",
       shQuote(normalizePath(system.file("/vbs/open_and_save_xl_workbooks.vbs", package="xprt"))),
       shQuote(normalizePath(u)),
       sep=" "
