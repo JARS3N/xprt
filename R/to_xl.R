ee89d653bc7950ce3bc34ad9a8bf14bae3750452
JARS3N <JARS3N@users.noreply.github.com>
2022-09-01 16:02:11 -0400

    Create to_xl.R

---
+++ R/to_xl.R
@@ -0,0 +1,12 @@
+to_xl<-function(u){
+ext <- tools::file_ext(u)
+loc <- c("asyr" = "Seahorse Bioscience\\Wave Desktop\\" ,
+         "xflr" = "Agilent Technologies\\Wave Pro\\"
+         )
+M <- shQuote(paste0("C:\\Program Files (x86)\\",loc[ext],"MassAssayExporter.exe"))
+dir <- shQuote(dirname(u))
+fl <-basename(u)
+fl2 <- gsub(ext,"xlsx",fl)
+str <- paste("cd",dir,"&&",M,fl,fl2,sep = " ")
+shell(str)
+}
